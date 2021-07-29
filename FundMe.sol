d43919e1e6a8673fab9cbbdd32d206c364dbd8e1
Patrick Collins <54278053+PatrickAlphaC@users.noreply.github.com>
2021-07-29 15:54:51 -0400

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -17,7 +17,7 @@ contract FundMe {
     }
     
     function fund() public payable {
-        uint256 mimimumUSD = 50 * 10 * 18;
+        uint256 mimimumUSD = 50 * 10 ** 18;
         require(getConversionRate(msg.value) >= mimimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);

db7df08e3937865e8dcd1300c182ab4627e01604
Ronak Shah <ronak.shah@outlook.com>
2021-12-29 22:32:12 -0500

    minimumUSD should use eth price
    The current minimumUSD price doesn't take in to account the eth <-> USD exchange rate. This should be updated to call getPrice().

--- FundMe.sol
+++ FundMe.sol
@@ -17,7 +17,7 @@ contract FundMe {
     }
     
     function fund() public payable {
-        uint256 minimumUSD = 50 * 10 ** 18;
+        uint256 minimumUSD = (50 / getPrice()) * 10 ** 18;
         require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);

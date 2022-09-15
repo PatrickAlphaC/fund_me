6beb984d7ed43971ce0195da787f02360e587725
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-09-15 14:56:01 -0400

    updated address for goerli

--- FundMe.sol
+++ FundMe.sol
@@ -5,79 +5,94 @@
 pragma solidity >=0.6.6 <0.9.0;
 
 // Get the latest ETH/USD price from chainlink price feed
+
+// IMPORTANT: This contract has been updated to use the Goerli testnet
+// Please see: https://docs.chain.link/docs/get-the-latest-price/
+// For more information
+
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
-	// safe math library check uint256 for integer overflows
+    // safe math library check uint256 for integer overflows
     using SafeMathChainlink for uint256;
-    
+
     //mapping to store which address depositeded how much ETH
     mapping(address => uint256) public addressToAmountFunded;
     // array of addresses who deposited
     address[] public funders;
     //address of the owner (who deployed the contract)
     address public owner;
-    
+
     // the first person to deploy the contract is
     // the owner
     constructor() public {
         owner = msg.sender;
     }
-    
+
     function fund() public payable {
-    	// 18 digit number to be compared with donated amount 
-        uint256 minimumUSD = 50 * 10 ** 18;
+        // 18 digit number to be compared with donated amount
+        uint256 minimumUSD = 50 * 10**18;
         //is the donated amount less than 50USD?
-        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "You need to spend more ETH!"
+        );
         //if not, add to mapping and funders array
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
-    
+
     //function to get the version of the chainlink pricefeed
-    function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
         return priceFeed.version();
     }
-    
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,int256 answer,,,) = priceFeed.latestRoundData();
-         // ETH/USD rate in 18 digit 
-         return uint256(answer * 10000000000);
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
     }
-    
+
     // 1000000000
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         // the actual ETH/USD conversation rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
-    
+
     //modifier: https://medium.com/coinmonks/solidity-tutorial-all-about-modifiers-a86cf81c14cb
-    modifier onlyOwner {
-    	//is the message sender owner of the contract?
+    modifier onlyOwner() {
+        //is the message sender owner of the contract?
         require(msg.sender == owner);
-        
+
         _;
     }
-    
-    // onlyOwner modifer will first check the condition inside it 
-    // and 
-    // if true, withdraw function will be executed 
-    function withdraw() payable onlyOwner public {
-    
-    	// If you are using Solidity version v0.8.0 or above,
-	// you will need to modify the code below to
-	// payable(msg.sender).transfer(address(this).balance);
+
+    // onlyOwner modifer will first check the condition inside it
+    // and
+    // if true, withdraw function will be executed
+    function withdraw() public payable onlyOwner {
         msg.sender.transfer(address(this).balance);
 
-        
         //iterate through all the mappings and make them 0
         //since all the deposited amount has been withdrawn
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
@@ -85,6 +100,3 @@ contract FundMe {
         funders = new address[](0);
     }
 }
-
-
-
